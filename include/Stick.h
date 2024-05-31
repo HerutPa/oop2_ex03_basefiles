42abd6e9c0bdd95b2f05e8d74a8c0b6eb04a940e
Herut Partner <herutpa@std.hac.ac.il>
2024-05-31 20:43:24 +0200

    cfdf

--- include/Stick.h
+++ include/Stick.h
@@ -38,6 +38,10 @@ private:
     sf::FloatRect m_bounds;
     
     void setColor(const Colors);
+    static bool doIntersect(Point p1, Point q1, Point p2, Point q2);
+    bool onSegment(Point p, Point q, Point r);
+    int orientation(Point p, Point q, Point r);
+
    
 
 };
--- src/Stick.cpp
+++ src/Stick.cpp
@@ -2,6 +2,7 @@
 #include "Stick.h"
 
 
+
 Stick::Stick(const int row, const int length)
 //	:  m_index(row)
 {
@@ -63,6 +64,65 @@ void Stick::setColor(const Colors color)
 	m_stick.setFillColor(Resources::instance().getColorArray()[color]);
 }
 
+static bool doIntersect(Point p1, Point q1, Point p2, Point q2);
+{
+	// Find the four orientations needed for general and special cases
+	int o1 = orientation(p1, q1, p2);
+	int o2 = orientation(p1, q1, q2);
+	int o3 = orientation(p2, q2, p1);
+	int o4 = orientation(p2, q2, q1);
+
+	// General case
+	if (o1 != o2 && o3 != o4)
+	{
+		return true;
+	}
+
+	// Special Cases
+	// p1, q1, and p2 are collinear and p2 lies on segment p1q1
+	if (o1 == 0 && onSegment(p1, p2, q1))
+	{
+		return true;
+	}
+	// p1, q1, and q2 are collinear and q2 lies on segment p1q1
+	if (o2 == 0 && onSegment(p1, q2, q1))
+	{
+		return true;
+	}
+	// p2, q2, and p1 are collinear and p1 lies on segment p2q2
+	if (o3 == 0 && onSegment(p2, p1, q2))
+	{
+		return true;
+	}
+	// p2, q2, and q1 are collinear and q1 lies on segment p2q2
+	if (o4 == 0 && onSegment(p2, q1, q2))
+	{
+		return true;
+	}
+
+	// No special case applies
+	return false;
+}
+
+bool Stick::onSegment(Point p, Point q, Point r)
+{
+	if (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) &&
+		q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y)) {
+		return true;
+	}
+	return false;
+}
+
+int Stick::orientation(Point p, Point q, Point r)
+{
+	int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+	if (val == 0) {
+		return 0;  // collinear
+	}
+	return (val > 0) ? 1 : 2; // clock or counterclockwise
+}
+
+
 bool Stick::isOverlaped(const Stick& stick1, const Stick& stick2)
 {
 	Point p1 = { stick1.getLocation().x, stick1.getLocation().y };
@@ -77,7 +137,6 @@ bool Stick::isOverlaped(const Stick& stick1, const Stick& stick2)
 
 	return doIntersect(p1, q1, p3, q3) || doIntersect(p1, q1, p4, q4) || doIntersect(p2, q2, p3, q3) || doIntersect(p2, q2, p4, q4);
 }
-
 
 void Stick::addOverLapped(const std::shared_ptr<Stick> &overlap)
 {
